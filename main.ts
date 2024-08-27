d7afbaebd7705e92cd7e45906f0faa76a20ad76e
Pollux96 <110056689+Pollux96@users.noreply.github.com>
2024-08-27 11:20:58 +0200

    Fix Error 989

--- main.ts
+++ main.ts
@@ -15,14 +15,20 @@ input.onButtonPressed(Button.A, function () {
     I2C_LCD1602.clear()
     I2C_LCD1602.ShowString("Ampel aus", 0, 0)
 })
-input.onButtonPressed(Button.AB, function () {
-    I2C_LCD1602.ShowString("Temperatur", 0, 0)
-    I2C_LCD1602.ShowNumber(input.temperature(), 12, 0)
-})
+function Temperatur () {
+    while (true) {
+        I2C_LCD1602.ShowString("Temperatur", 0, 0)
+        I2C_LCD1602.ShowNumber(input.temperature(), 12, 0)
+    }
+}
 let strip: neopixel.Strip = null
 I2C_LCD1602.LcdInit(39)
+I2C_LCD1602.ShowString("Z()ZN&876ztbzzghzuhizuhzthhtut6", 0, 0)
+basic.pause(200)
+I2C_LCD1602.clear()
 I2C_LCD1602.ShowString("Starting", 1, 0)
 basic.pause(500)
+I2C_LCD1602.clear()
 let strip1 = neopixel.create(DigitalPin.P14, 24, NeoPixelMode.RGB)
 strip = neopixel.create(DigitalPin.P14, 4, NeoPixelMode.RGB)
 strip.showRainbow(1, 4)
@@ -51,11 +57,14 @@ strip1.showRainbow(1, 360)
 basic.pause(2000)
 I2C_LCD1602.clear()
 basic.pause(200)
+Temperatur()
+basic.forever(function () {
+    strip1.rotate(1)
+    basic.pause(100)
+    strip1.show()
+})
 basic.forever(function () {
     strip.rotate(1)
     basic.pause(100)
     strip.show()
-    strip1.rotate(1)
-    basic.pause(100)
-    strip1.show()
 })
